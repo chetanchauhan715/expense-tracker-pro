bc0fd1541ecc7976c364a95ac07792d2e575e319
Chetan Chauhan <chetan99256@gmail.com>
2026-06-15 16:16:17 +0530

    feat: add application header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import "./Header.css";
+function  Header (){
+    return(
+        <header className="header">
+            <h1>Expense Tracker Pro</h1>
+
+            <p>Track your income and expenses with ease.</p>
+        </header>
+    );
+}
+export default Header;
