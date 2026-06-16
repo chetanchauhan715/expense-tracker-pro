50f2107667c38cf2c13a6cdb92f816742f88f320
Chetan Chauhan <chetan99256@gmail.com>
2026-06-16 16:46:20 +0530

    feat: Implement controlled transaction form with reusable input handler

---
+++ src/components/TransactionForm/TransactionForm.jsx
@@ -0,0 +1,94 @@
+import { useState } from "react";
+import "./TransactionForm.css";
+
+function TransactionForm (){
+
+    const [formData , setFormData] = useState ( {
+        title:"",
+        amount:"",
+        category:"",
+        date:"",
+        type:"expense"
+    });
+
+    function handleChange(e){
+        setFormData({
+            ...formData , 
+            [e.target.name] : e.target.value
+        }
+
+        );
+    }
+
+    return(
+        <section className="form">
+            <form>
+                <input
+                type="text"
+                placeholder="Enter Title"
+                name = "title"
+                value={formData.title}
+                onChange={handleChange}
+                />
+
+
+                <input
+                type="number"
+                placeholder="Enter Amount"
+                name="amount"
+                value={formData.amount}
+                onChange={handleChange}
+                />
+
+                <select 
+                name="category"
+                value={formData.category}
+                onChange={handleChange}
+                >
+                    <option value="">Select Category</option>
+                    <option value="food">Food</option>
+                    <option value="transport">Traansport</option>
+                    <option value="shopping">Shopping</option>
+                    <option value="bills">Bills</option>
+                    <option value="salary">Salary</option>
+                    <option value="entertainmnet">Entertainment</option>
+                    <option value="heathcare">HeathCare</option>
+                    <option value="other">Other</option>
+                </select>
+
+
+                <input
+                type="date"
+                name="date"
+                value={formData.date}
+                onChange={handleChange}
+                />
+
+                <label>
+                <input
+                type="radio"
+                name="type"
+                value="income"
+                onChange={handleChange}
+                checked={formData.type === "income"} 
+                />
+                Income
+                </label>
+
+                <label>
+                    <input 
+                    type="radio"
+                    name="type"
+                    value="expense"
+                    onChange={handleChange}
+                    checked={formData.type === "expense"}
+                    />
+                    Expense
+                </label>
+
+            </form>
+        </section>
+    );
+}
+
+export default TransactionForm;
