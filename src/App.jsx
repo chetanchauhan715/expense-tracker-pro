c24698a26e8169b1da7dd56e33b40c75a21f47b3
Chetan Chauhan <chetan99256@gmail.com>
2026-06-18 17:27:34 +0530

    feat:add delete transaction function

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,59 @@
 import { useState } from 'react'
-
+import Header from './components/Header/Header';
+import Dashboard from './components/Dashboard/Dashboard';
+import StatsCard from './components/Dashboard/StatsCard';
+import TransactionForm from './components/TransactionForm/TransactionForm';
+import TransactionList from './components/TransactionList/TransactionList';
 
 function App() {
+
+  const [transactions , setTransactions] = useState([]);
+
+  function addTransaction(newTransaction){
+    
+    const transaction = {
+      ...newTransaction , 
+      id:Date.now()
+    } ;
+
+   setTransactions( (prev)=>[
+    ...prev , transaction
+   ]);
+  }
+
+  function delelteTransaction(id){
+   const updatedTrasactions =  transactions.filter( (transaction)=> (
+      transaction.id !== id 
+    ))
+
+    setTransactions(updatedTrasactions);
+  }
  
-  return <h1>Expense Tracker Pro</h1>;
-}
+  return (
+
+    <div>
+    
+    <Header/>
+
+    <Dashboard transactions={transactions}/>
+
+
+<div>
+    <TransactionForm 
+    onAddTransaction ={addTransaction}
+    />
+
+    
+</div>
+
+<TransactionList transactions={transactions}
+                onDeleteTransaction={delelteTransaction}
+/>
+
+    
+
+    </div>
+  ) ;
+} 
 
 export default App;
