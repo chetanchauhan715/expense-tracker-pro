e8f65296800fad5655860ff79221cb52da96aaab
Chetan Chauhan <chetan99256@gmail.com>
2026-06-24 10:08:16 +0530

    refactor: change manual to props based transactions statistics

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -2,16 +2,36 @@ import StatsCard from "./StatsCard";
 import "./Dashboard.css";
 
 
-function Dashboard() {
+function Dashboard({totalIncome , totalExpense , balance , totalTransactions}) {
+
+  // const income = transactions.reduce( (acc,transaction) => {
+  //   if(transaction.type === "income"){
+  //     return acc + Number(transaction.amount);
+  //   }
+  //   return acc;
+  // } , 0);
+
+
+  // const expense = transaction s.reduce( (acc, transaction)=>{
+  //   if(transaction.type === "expense"){
+  //     return acc + Number(transaction.amount);
+  //   }
+  //   return acc;
+  // }, 0) ;
+
+  // const balance = income - expense;
+
+  // const totalTransactions = transactions.length ;
+
   return (
     <section className="dashboard">
-      <StatsCard title="Balance" value="$25,000" />
+      <StatsCard title="Balance" value={balance} />
 
-      <StatsCard title="Income" value="$75,000" />
+      <StatsCard title="Income" value={totalIncome} />
 
-      <StatsCard title="Expense" value="$50,000" />
+      <StatsCard title="Expense" value= {totalExpense} />
 
-      <StatsCard title="Savings" value="$25,000" />
+      <StatsCard title="Total Transactions" value={totalTransactions} />
     </section>
   );
 }
